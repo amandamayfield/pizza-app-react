c6b106a27e512a18de39a78d59ab55ac043051a5
amandamayfield <amandamayfield.designs@gmail.com>
2025-12-20 15:36:48 -0600

    init project

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "Pepperoni",
+      description: "A classic pizza with pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "Veggie",
+      description: "A pizza with a variety of vegetables",
+    }),
+    React.createElement(Pizza, {
+      name: "Hawaiian",
+      description: "A pizza with ham and pineapple",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(React.createElement(App));
