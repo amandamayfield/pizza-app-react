f0becd1ad34859aed3a882eaa32aafd9de9660a2
amandamayfield <amandamayfield.designs@gmail.com>
2026-01-05 19:28:16 -0600

    feat: updated forms to use react server actions and use form status

--- src/routes/contact.lazy.jsx
+++ src/routes/contact.lazy.jsx
@@ -1,5 +1,6 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
 import { useMutation } from "@tanstack/react-query";
+import { useFormStatus } from "react-dom";
 import postContact from "../api/postContact";
 
 export const Route = createLazyFileRoute("/contact")({
@@ -8,9 +9,7 @@ export const Route = createLazyFileRoute("/contact")({
 
 function ContactRoute() {
   const mutation = useMutation({
-    mutationFn: function (e) {
-      e.preventDefault();
-      const formData = new FormData(e.target);
+    mutationFn: function (formData) {
       return postContact(
         formData.get("name"),
         formData.get("email"),
@@ -25,13 +24,26 @@ function ContactRoute() {
       {mutation.isSuccess ? (
         <h3>Submitted!</h3>
       ) : (
-        <form onSubmit={mutation.mutate}>
-          <input name="name" placeholder="Name" />
-          <input type="email" name="email" placeholder="Email" />
+        <form action={mutation.mutate}>
+          <ContactInput name="name" type="text" placeholder="Name" />
+          <ContactInput name="email" type="email" placeholder="Email" />
           <textarea placeholder="Message" name="message"></textarea>
           <button>Submit</button>
         </form>
       )}
     </div>
+  );
+}
+
+function ContactInput(props) {
+  // you can pull in the the form status to disable inputs while pending
+  const { pending } = useFormStatus();
+  return (
+    <input
+      disabled={pending}
+      name={props.name}
+      type={props.type}
+      placeholder={props.placeholder}
+    />
   );
 }
